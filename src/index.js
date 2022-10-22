c9d9f1263c635892e57f70fd41f084b75aa30332
Andrés Weitzel <andres96energy@hotmail.com>
2022-10-22 20:49:28 -0300

    Removed gitignore files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+
+const app = express()
+
+
+app.get('/', (req, res) =>{
+    res.send('Hello World!!');
+});
+
+app.listen(3100)
+console.log('Servidor en Ejecución en el Puerto ', 3100)
+
+
+
