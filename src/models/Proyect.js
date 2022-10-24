fd9c327fb1ec4f65afd04bf759d6940e7a92a89e
Andrés Weitzel <andres96energy@hotmail.com>
2022-10-23 21:31:09 -0300

    Updated data model

--- src/models/Proyect.js
+++ src/models/Proyect.js
@@ -1,24 +1,107 @@
 import { Schema, model } from 'mongoose'
 
+//data schema
+const proyectSchema = new Schema({
+    proyecto_id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    proyecto_fuente: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    titulo: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    fecha_inicio: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    fecha_finalizacion: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    resumen: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    moneda_id: {
+        type: Number,
+        required: false,
+        unique: false
+    },
+    monto_total_solicitado:  {
+        type: Number,
+        required: true,
+        unique: false
+    },
+    monto_total_adjudicado: {
+        type: Number,
+        required: true,
+        unique: false
+    },
+    monto_financiado_solicitado: {
+        type: Number,
+        required: true,
+        unique: false
+    },
+    monto_financiado_adjudicado: {
+        type: Number,
+        required: true,
+        unique: false
+    },
+    tipo_proyecto_id: {
+        type: Number,
+        required: false,
+        unique: false
+    },
+    codigo_identificacion: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    palabras_clave: {
+        type: String,
+        required: false,
+        unique: false
+    },
+    estado_id:  {
+        type: Number,
+        required: false,
+        unique: false
+    },
+    fondo_anpcyt:  {
+        type: String,
+        required: false,
+        unique: false
+    },
+    cantidad_miembros_F:  {
+        type: Number,
+        required: false,
+        unique: false
+    },
+    cantidad_miembros_M:  {
+        type: Number,
+        required: false,
+        unique: false
+    },
+    sexo_director:  {
+        type: String,
+        required: false,
+        unique: false
+    },
+},{
+    versionKey:false
+})
 
-proyect
-    proyecto_id
-    proyecto_fuente
-    titulo
-    fecha_inicio
-    fecha_finalizacion
-    resumen
-    moneda_id
-    monto_total_solicitado
-    monto_total_adjudicado
-    monto_financiado_solicitado
-    monto_financiado_adjudicado
-    tipo_proyecto_id
-    codigo_identificacion
-    palabras_clave
-    estado_id
-    fondo_anpcyt
-    cantidad_miembros_F
-    cantidad_miembros_M
-    sexo_director
+//data model
+export default model('Proyect',proyectSchema)
+
 
